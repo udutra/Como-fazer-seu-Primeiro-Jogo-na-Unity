f79e7fa88bbf3a06e000a35628785c9acf222400
Guilherme Galarça Dutra <udutra@gmail.com>
2021-09-21 21:30:44 -0300

    Criação da UI.
    TxtPontuacao e painel de GameOVer.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
+    
+    private Vector3 posicaoInicial;
+    public GameObject painelGameOver;
+    public Text txtPontuacao;
+    public PlayerController player;
+    public float divisorPontuacao;
+    
+    private void Start() {
+        posicaoInicial = player.transform.position;
+    }
 
+    private void Update() {
+        Vector3 distanciaPercorrida = player.transform.position - posicaoInicial;
+        float pontuacao = distanciaPercorrida.z / divisorPontuacao;
+        txtPontuacao.text = pontuacao.ToString("0");
+    }
 
     public void GameOver() {
+        painelGameOver.SetActive(true);
+        Invoke("RecarregarLevel", 2);
+    }
+
+    private void RecarregarLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
